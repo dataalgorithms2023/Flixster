3bd696434092d2e36d46b8de976b3782de6e3b3a
Donald-Morgan <71024885+Dondavi15574@users.noreply.github.com>
2023-05-05 20:21:06 -0400

    Initial commit

---
+++ app/src/main/res/layout/movies.kt
@@ -0,0 +1,9 @@
+package layout
+
+class movies {@SerializedName("title")
+private val title: String? = null
+    @SerializedName("overview")
+    private val overview: String? = null
+    @SerializedName("poster_path")
+    private val img: String? = null
+}
